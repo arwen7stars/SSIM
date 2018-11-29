d4ace10512f415a15d53ee2527a887e09e1bb814
tirafesi <tirafesi@hotmail.com>
2018-11-29 19:57:32 +0000

    initial room setup

---
+++ Assets/Scripts/aiming.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aiming : MonoBehaviour {
+
+	// the player model transform
+	private Transform player;
+
+	// Use this for initialization
+	void Start () {
+		player = transform.Find("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// cast ray
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		// aim towards hit object
+		if (Physics.Raycast(ray, out hit)) {
+			player.LookAt(hit.point);
+			Debug.DrawLine(ray.origin, hit.point, Color.red);
+		}
+	}
+}
