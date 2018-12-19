d372abb8affccbc62f206639f3e0fa28996f6e4b
Cláudia Marinho <claudia.m.r.marinho@gmail.com>
2018-12-19 14:52:47 +0000

    added tags to ducks

--- Assets/Scripts/Player/PlayerFiring.cs
+++ Assets/Scripts/Player/PlayerFiring.cs
@@ -5,6 +5,9 @@ public class PlayerFiring : StateMachineBehaviour {
 	// the player controller
 	private PlayerController player;
 
+    // duck tag
+    private const string DUCK_TAG = "Duck";
+
 
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		player = GameObject.Find("Player").GetComponent<PlayerController>();
@@ -14,7 +17,13 @@ public class PlayerFiring : StateMachineBehaviour {
 		RaycastHit hit;
 		if (Physics.Raycast(player.GetRay(), out hit)) {
 			Debug.Log("target hit: " + hit.transform.name);
-		}	
+
+            if (hit.transform.tag == DUCK_TAG)
+            {
+                Debug.Log("I hit a duck");
+                // increase score
+            }
+        }	
 	}
 
 
